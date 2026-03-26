86c57f9b6afac6674891ede6146595d451c5a4d4
KiranKumar <kirankumar.gontela@gmail.com>
2026-03-26 07:39:25 +0530

    ..

--- app.py
+++ app.py
@@ -26,11 +26,16 @@ model = load_model()
 # -----------------------------
 # Class Names (CHANGE if needed)
 # -----------------------------
-CLASS_NAMES = [
-    "Healthy",
-    "Early Blight",
-    "Late Blight"
-]
+CLASS_NAMES =['Tomato_Bacterial_spot',
+ 'Tomato_Early_blight',
+ 'Tomato_Late_blight',
+ 'Tomato_Leaf_Mold',
+ 'Tomato_Septoria_leaf_spot',
+ 'Tomato_Spider_mites_Two_spotted_spider_mite',
+ 'Tomato__Target_Spot',
+ 'Tomato__Tomato_YellowLeaf__Curl_Virus',
+ 'Tomato__Tomato_mosaic_virus',
+ 'Tomato_healthy']
 
 # -----------------------------
 # Image Preprocessing
